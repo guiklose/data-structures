fdfb121c1961df094c5b801a275484fb79134d7a
guiklose <guilhermeklosesantos@gmail.com>
2025-09-21 21:34:40 -0300

    Changed graph class and translated it to english

--- graphs/graph_class.py
+++ graphs/graph_class.py
@@ -1,87 +1,85 @@
 class Graph:
   def __init__(self):
-    self.__qtd_vertices = 0
-    self.__qtd_arestas = 0
+    self.__vertice_count = 0
+    self.__edge_count = 0
     self.__table = []
-    self.__rotulos = []
+    self.__labels = []
 
   def get_table(self):
     return self.__table
 
-  # O(1) - Assegurado por atributo extra '__qtd_vertices'
-  def qtdVertices(self):
-    return self.__qtd_vertices
+  # O(1)
+  def vertice_count(self):
+    return self.__vertice_count
 
-  # O(1) - Assegurado por atributo extra '__qtd_arestas'
-  def qtdArestas(self):
-    return self.__qtd_arestas
+  # O(1)
+  def edge_count(self):
+    return self.__edge_count
 
-  # O(n) n sendo a quantidade de vertices.
-  def grau(self, v):
+  # O(n)
+  def degree(self, v):
+    if v == float("inf"):
+      return 0
+
     quantity_vertices = 0
     for element in self.__table[v - 1]:
-      if (element):
+      if (element != float("inf")):
         quantity_vertices += 1
     return quantity_vertices
 
-  # O(n) n sendo a quantidade de vertices.
-  def vizinhos(self, v):
+  # O(n)
+  def get_neightbors(self, v):
     vizinhos = []
     index = 0
 
     for vertice in self.__table[v - 1]:
       index += 1
 
-      if vertice != float('inf'):
+      if vertice != float("inf"):
         vizinhos.append(index)
 
     return vizinhos
 
   # O(1)
-  def haAresta(self, u, v):
-    if (self.__table[u - 1][v - 1]):
-      return True
-    return False
+  def has_edge(self, u, v):
+    return self.__table[u - 1][v - 1] != float("inf")
 
   # O(1)
-  def peso(self, u, v):
+  def weight(self, u, v):
     return self.__table[u - 1][v - 1]
 
-  # O(n) - Depende do número de linhas de entrada.
-  def ler(self, arquivo):
-    file = open(arquivo, 'r', encoding='utf-8')
+  # O(n)
+  def read(self, path):
+    file = open(path, "r", encoding="utf-8")
 
     content = file.readlines()
 
-    # Get vertices length
     len_vertice = int(content.pop(0).split()[1])
 
-    # '+1' é para se livrar do '*edges'
     for i in range(len_vertice + 1):
       vertice_info = content.pop(0).split()
 
-      if vertice_info[0] == '*edges':
+      if vertice_info[0] == "*edges":
         break
 
-      # Populando numero de linhas da tabela a.k.a vertices
       self.__table.append([])
-      # Salvando em estrutura separada os rotulos
-      self.__rotulos.append((vertice_info[1], self.qtdVertices()))
-      # Atualizando info da quantidade de vertices
-      self.__qtd_vertices += 1
+      self.__labels.append((vertice_info[1], self.vertice_count()))
+      self.__vertice_count += 1
 
       for i in range(len_vertice):
-        self.__table[self.qtdVertices() - 1].append(float('inf'))
+        self.__table[self.vertice_count() - 1].append(float("inf"))
 
-    # Populando o Grafo com valores de arestas
     for l in range(len(content)):
-      aresta_info = content.pop(0).split()
-      aresta_start = int(aresta_info[0])
-      aresta_final = int(aresta_info[1])
+      edge_info = content.pop(0).split()
 
-      self.__table[aresta_start - 1][aresta_final - 1] = int(aresta_info[2])
-      # Grafos não dirigidos tem relação mutua.
-      self.__table[aresta_final - 1][aresta_start - 1] = int(aresta_info[2])
+      if edge_info:
+        start_edge = int(edge_info[0])
+        final_edge = int(edge_info[1])
 
-      # Atualizando info da quantidade de arestas
-      self.__qtd_arestas += 1
+        self.__table[start_edge - 1][final_edge - 1] = float(edge_info[2])
+        self.__table[final_edge - 1][start_edge - 1] = float(edge_info[2])
+
+        self.__edge_count += 1
+    else:
+      if self.__edge_count == 0:
+        self.__table = []
